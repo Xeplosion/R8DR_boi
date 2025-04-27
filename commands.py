046f54ea89785584017ea4a93d0f89082a99f000
Xeplosion <xeplosion@xegames.online>
2025-04-27 12:01:19 -0600

    Added command to retrieve information for bot connection in dict format.

--- commands.py
+++ commands.py
@@ -8,3 +8,14 @@ def get_random_verse(input_path):
             verses.append(line)
 
     return random.choice(verses)
+
+def load_info(filepath):
+    info = {}
+    with open(filepath, 'r', encoding='utf-8') as f:
+        for line in f:
+            if "=" in line:
+                key, value = line.strip().split("=", 1)
+                info[key] = value
+    
+    return info
+ 
