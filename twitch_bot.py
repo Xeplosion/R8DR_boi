8a76cb5abb9caf08b3804e8493678c542cf03c6a
Xeplosion <xeplosion@xegames.online>
2025-04-27 12:02:22 -0600

    Updated to use new dict data format.

--- twitch_bot.py
+++ twitch_bot.py
@@ -1,32 +1,38 @@
 import os
+import asyncio
 from twitchio.ext import commands
 
 from commands import get_random_verse
 
 class Bot(commands.bot):
-
+    # Construct the bot using our oauth token and channel info.
     def __init__(self):
-        # First get our 
-        INFO = []
-        with open("./token.txt", 'r', encoding='utf-8') as f:
-            for line in f:
-                INFO.append(line)
-
-        # Then we construct our bot class using that token
+        info = load_info("./information.txt")
         super().__init__(
-            token=INFO[0],
+            token=info['token'],
             prefix="!",
-            initial_channels=[INFO[1]]
+            initial_channels=info['channel'],
+            reconnect=True # Enable auto reconnect.
         )
 
+    # Avoids startup spam.
     async def event_ready(self):
         print(f'Logged in as | {self.nick}')
+        
+        # Wait a few seconds before starting up fully
+        await asyncio.sleep(2)  # 2 seconds, you can adjust
+        print("Bot is fully ready!")
+
+    # Handle disconnect events.
+    async def event_disconnect(self):
+        print("Bot disconnected! Attempting to reconnect...")
 
     # Randomly selects a verse from a text file to type in chat.
     @commands.command(name='verse')
     async def verse(self, ctx):
         verse = get_random_verse("./verses.txt")
         await ctx.send(verse)
+
 
 
 bot = Bot()
